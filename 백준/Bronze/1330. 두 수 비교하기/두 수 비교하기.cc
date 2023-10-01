febdacb66a68eda4ad106e0e8a0bda8fcc8fd94e
BAE KYUNGTAEK <125863048+GTAEKI@users.noreply.github.com>
2023-10-01 13:10:10 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    
+    if(a>b)
+    {
+        printf(">");
+    }
+    else if(a<b)
+    {
+        printf("<");
+    }
+    else if(a==b)
+    {
+        printf("==");
+    }
+    return 0;
+
+}
