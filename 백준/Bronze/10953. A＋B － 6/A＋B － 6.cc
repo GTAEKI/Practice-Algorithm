0439d73ae6331927fb279689d7d6568254c3c4ab
BAE KYUNGTAEK <125863048+GTAEKI@users.noreply.github.com>
2023-11-26 20:01:49 +0900

    [Bronze III] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t = 0, a = 0, b = 0;
+    char c;
+    
+    cin >> t ;
+    for(int i = 0 ; i < t ; i++)
+    {
+        cin >> a >> c >> b;
+        cout << a + b << "\n";
+    }
+}
