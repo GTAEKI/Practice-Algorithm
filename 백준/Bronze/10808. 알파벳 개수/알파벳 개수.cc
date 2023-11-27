24b1840735380a23531908438d659a1a1b076b97
BAE KYUNGTAEK <125863048+GTAEKI@users.noreply.github.com>
2023-11-27 17:31:03 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,13 +1,13 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
 using namespace std;
 
 string name;
+int answer[26];
 
 int main()
 {
     cin >> name;
-    
-    int answer[26] = {0,};
     
     for(int i = 0; i < name.length(); i++)
     {
