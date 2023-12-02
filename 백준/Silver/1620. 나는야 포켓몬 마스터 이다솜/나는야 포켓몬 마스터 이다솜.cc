fe8dcfd2724a8a7940bab8b192399f95c6bc6291
BAE KYUNGTAEK <125863048+GTAEKI@users.noreply.github.com>
2023-12-02 16:18:03 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 268 ms, Memory: 25528 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,m;
+map<string,int> mp1;
+map<int,string> mp2;
+string s;
+string temp;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> s;
+        mp1[s] = i+1;
+        mp2[i+1] = s;
+    }
+    
+    for(int i = 0; i < m; i++)
+    {
+        cin >> temp;
+        if(atoi(temp.c_str())==0)
+           {
+               cout << mp1[temp] << "\n";
+           }
+           else
+           {
+               cout << mp2[atoi(temp.c_str())] << "\n";
+           }
+    }
+           return 0;
+}
