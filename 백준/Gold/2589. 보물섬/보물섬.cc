c016b4e258c290c18be17de62b0106752f1c3d77
배경택(Bae Kyungtaek) <kyungtaek0204@gmail.com>
2024-03-22 11:54:35 +0900

    [Gold V] Title: 보물섬, Time: 112 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,67 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int H, W;
+int w,h,max_Length;
 const int MAX = 51;
-char mapp[MAX][MAX];      
-bool visited[MAX][MAX];
-int path[MAX][MAX];
-int dy[] = { 0,0,-1,1 };
-int dx[] = { -1,1,0,0 };
-queue<pair<int, int>> q;
-int max_len = 0;
+char gameMap[MAX][MAX];
+int visited[MAX][MAX];
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
 
-int BFS(int y, int x) {
-    path[y][x] = 1;
-    visited[y][x] = true;
-    q.push(make_pair(y, x));
- 
-    while (!q.empty()) {
-        y = q.front().first;
-        x = q.front().second;
-        q.pop();
- 
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
- 
-            if (ny < 0 || nx < 0 || ny >= H || nx >= W)
-                continue;
-            if (mapp[ny][nx] == 'L' && !visited[ny][nx]) {
-                visited[ny][nx] = true;
-                path[ny][nx] = path[y][x] + 1;
-                q.push(make_pair(ny, nx));
-                if (max_len < path[ny][nx]) {
-                    max_len = path[ny][nx];
-                }
-            }
+void bfs(int j, int i)
+{
+    memset(visited,0,sizeof(visited));
+    visited[j][i] = 1;
+    int nx,ny,y,x = 0;
+    queue<pair<int,int>> que;
+    que.push({j,i});
+    while(!que.empty())
+    {
+        tie(y,x) = que.front();
+        que.pop();
+        for(int i = 0; i < 4; i++)
+        {
+            ny = y + dy[i];
+            nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= h || nx >= w || visited[ny][nx]) continue;
+            if(gameMap[ny][nx] == 'W') continue;
+            visited[ny][nx] = visited[y][x] +1;
+            max_Length = max(visited[ny][nx],max_Length);
+            que.push({ny,nx});
         }
     }
-    return max_len - 1;
 }
 
-// 보물은 서로 간에 최단 거리로 이동하는데 있어 가장 긴 시간이 걸리는 육지 두곳에 나뉘어 묻혀있음
- 
-int main() {
-    cin >> H >> W;
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < W; j++) {
-            scanf("%1s", &mapp[i][j]);
+int main()
+{
+    cin >> h >> w;
+    for(int j = 0; j < h; j++)
+    {
+        for(int i = 0; i < w; i++)
+        {
+            cin >> gameMap[j][i];
         }
     }
     
-    int ans = 0; 
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < W; j++) {
-            if (mapp[i][j] == 'L') {
-                ans = max(BFS(i, j),ans);
-                memset(visited,0,sizeof(visited));
-                memset(path,0,sizeof(path));
-                max_len = 0;
-            }
+    for(int j = 0; j < h; j++)
+    {
+        for(int i = 0; i < w; i++)
+        {
+            if(gameMap[j][i] == 'L')
+            {
+                bfs(j,i);
+            }         
         }
     }
- 
-    cout << ans;
+    
+    cout << max_Length - 1;
     return 0;
 }
