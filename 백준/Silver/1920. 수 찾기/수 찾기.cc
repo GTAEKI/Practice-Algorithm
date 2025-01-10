5c1356f6d53dea8457ad349c0bbbb7638b7bf2f3
배경택(Bae Kyungtaek) <kyungtaek0204@gmail.com>
2025-01-10 16:31:26 +0900

    [Silver IV] Title: 수 찾기, Time: 60 ms, Memory: 6820 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -7,20 +7,20 @@ int main()
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL); cout.tie(NULL);
     
-    unordered_map<int,int> arrM;
+    unordered_set<int> arrM;
     
     cin >> n;
     for(int i = 0; i < n; i++)
     {
         cin >> temp;
-        arrM[temp] = 1;
+        arrM.insert(temp);
     }
     
     cin >> m;
     for(int i = 0; i < m; i++)
     {
         cin >> temp;
-        if(arrM[temp] == 1)
+        if(arrM.find(temp) != arrM.end())
             cout << 1 << "\n";
         else
             cout << 0 << "\n";
